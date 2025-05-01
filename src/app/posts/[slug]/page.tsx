24c93eaa1b519e13717fa5eab6b33edddf292f6b
sootopia <shpark7502@naver.com>
2025-05-01 23:21:27 +0900

    feat: 읽기 추정 시간 추가

--- src/app/posts/[slug]/page.tsx
+++ src/app/posts/[slug]/page.tsx
@@ -2,6 +2,7 @@ import { supabase } from '@/lib/supabase';
 import { notFound } from 'next/navigation';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import readingTime from 'reading-time';
 
 type Post = {
   id: string;
@@ -29,6 +30,8 @@ export default async function Post({ params }: { params: { slug: string } }) {
     notFound();
   }
 
+  const readingMinutes = parseInt(Number(readingTime(post.content).minutes).toFixed(0));
+
   const formatDate = (date: string) => {
     const dateObj = new Date(date);
     const year = dateObj.getFullYear();
@@ -50,6 +53,14 @@ export default async function Post({ params }: { params: { slug: string } }) {
               </span>
               <span className="text-gray-400 mx-1">・</span>
               <span className="text-gray-500 text-sm">{formatDate(post?.created_at)}</span>
+              <span className="text-gray-400 mx-1">・</span>
+              <span className="text-gray-500 text-sm inline-flex items-center gap-1">
+                <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" viewBox="0 0 16 16">
+                  <path d="M8 3.5a.5.5 0 0 0-1 0V9a.5.5 0 0 0 .252.434l3.5 2a.5.5 0 0 0 .496-.868L8 8.71z" />
+                  <path d="M8 16A8 8 0 1 0 8 0a8 8 0 0 0 0 16m7-8A7 7 0 1 1 1 8a7 7 0 0 1 14 0" />
+                </svg>
+                {readingMinutes}분
+              </span>
             </div>
           </div>
           <div className="prose max-w-none">
