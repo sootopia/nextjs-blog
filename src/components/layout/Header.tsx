5e24d253b1ce0b2f1ef36e47104c6c893b5a5bf2
sootopia <shpark7502@naver.com>
2025-05-01 23:10:25 +0900

    feat: 헤더 컴포넌트에서 최신 작성글 및 수정글 데이터 비동기 처리로 변경

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,59 +1,20 @@
-'use client';
 import Link from 'next/link';
 import { supabase } from '@/lib/supabase';
-import { useState, useEffect } from 'react';
 
-type Post = {
-  slug: string;
-};
+export default async function Header() {
+  const { data: latestPost } = await supabase
+    .from('posts')
+    .select('slug')
+    .order('created_at', { ascending: false })
+    .limit(1)
+    .single();
 
-export default function Header() {
-  const [latestPostSlug, setLatestPostSlug] = useState<Post | null>(null);
-  const [latestUpdatedPostSlug, setLatestUpdatedPostSlug] = useState<Post | null>(null);
-
-  useEffect(() => {
-    const fetchLatestPost = async () => {
-      try {
-        const { data, error } = await supabase
-          .from('posts')
-          .select('slug')
-          .order('created_at', { ascending: false })
-          .limit(1);
-
-        if (error) {
-          throw error;
-        }
-
-        setLatestPostSlug(data[0].slug || null);
-      } catch (error) {
-        console.error('최신 작성글 불러오기 실패 : ', error);
-      }
-    };
-
-    fetchLatestPost();
-  }, []);
-
-  useEffect(() => {
-    const fetchLatestUpdatedPost = async () => {
-      try {
-        const { data, error } = await supabase
-          .from('posts')
-          .select('slug')
-          .order('updated_at', { ascending: false })
-          .limit(1);
-
-        if (error) {
-          throw error;
-        }
-
-        setLatestUpdatedPostSlug(data[0].slug || null);
-      } catch (error) {
-        console.error('최신 수정글 불러오기 실패 : ', error);
-      }
-    };
-
-    fetchLatestUpdatedPost();
-  }, []);
+  const { data: latestUpdatedPost } = await supabase
+    .from('posts')
+    .select('slug')
+    .order('updated_at', { ascending: false })
+    .limit(1)
+    .single();
 
   return (
     <header className="fixed top-0 left-0 right-0 h-[70px] border-b border-gray-200">
@@ -70,17 +31,17 @@ export default function Header() {
               </Link>
             </li>
 
-            {latestPostSlug && (
+            {latestPost?.slug && (
               <li>
-                <Link href={`/posts/${latestPostSlug}`} className="text-sm text-gray-700 hover:opacity-50">
+                <Link href={`/posts/${latestPost.slug}`} className="text-sm text-gray-700 hover:opacity-50">
                   최신 작성글
                 </Link>
               </li>
             )}
 
-            {latestUpdatedPostSlug && (
+            {latestUpdatedPost?.slug && (
               <li>
-                <Link href={`/posts/${latestUpdatedPostSlug}`} className="text-sm text-gray-700 hover:opacity-50">
+                <Link href={`/posts/${latestUpdatedPost.slug}`} className="text-sm text-gray-700 hover:opacity-50">
                   최신 수정글
                 </Link>
               </li>
